c6263e7483fc69d189e373f8c2c1b517282ecd76
mariopweb <mariopweb@gmail.com>
2020-06-11 09:00:43 +0200

    language files update, add debug position, J!icon system margin fix

--- templates/elementary/index.php
+++ templates/elementary/index.php
@@ -245,6 +245,7 @@ if (!$this->countModules('footer')) {
             </div>
         </footer>
     </div>
+    <jdoc:include type="modules" name="debug" style="none" />
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
